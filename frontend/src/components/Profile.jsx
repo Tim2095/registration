a5896a8423a09f88a4233423ec7d0043203cae0d
Timur <timur.mosenkov@gmail.com>
2024-08-13 21:34:04 +0300

    add edit button and redirect

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,6 +1,13 @@
 import { useSelector } from "react-redux";
 import classes from "./profile.module.scss";
+import { useNavigate } from "react-router-dom";
 const Profile = () => {
+  const navigate = useNavigate()
+
+  const onEdit = () => {
+    navigate('/edit')
+  }
+
   const user = useSelector((state) => state.users);
   return (
     <div className={classes.container}>
@@ -10,7 +17,7 @@ const Profile = () => {
       <h2>Your name: {user.name}</h2>
       <h2>Your username: {user.username}</h2>
       <h2>Your age: {user.age}</h2>
-      <button className={classes.button}>Edit</button>
+      <button onClick={onEdit} className={classes.button}>Edit</button>
     </div>
   );
 };
