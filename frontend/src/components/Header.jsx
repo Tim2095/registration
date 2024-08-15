b1574a052c33473f01f8d15dac7cbe7aa764a0a4
Timur <timur.mosenkov@gmail.com>
2024-08-15 19:02:21 +0300

    add profile link to header in case user logged in

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,6 @@ import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { clearUser } from "../reducers/userReducer";
 
-
 const Header = () => {
   const user = useSelector((state) => state.users);
   const navigate = useNavigate();
@@ -11,24 +10,32 @@ const Header = () => {
 
   const handleSignOut = (e) => {
     e.preventDefault();
-    localStorage.removeItem('authAppUser');
-    dispatch(clearUser()); 
-    navigate('/');
+    localStorage.removeItem("authAppUser");
+    dispatch(clearUser());
+    navigate("/");
   };
 
   return (
     <header className={classes.header}>
       <div className={classes["header-links"]}>
-        <li className={classes["header-link"]}>
-          <Link to="/register">SignUp</Link>
-        </li>
+        {!user ? (
+          <li className={classes["header-link"]}>
+            <Link to="/register">SignUp</Link>
+          </li>
+        ) : (
+          <li className={classes["header-link"]}>
+            <Link to="/profile">Profile</Link>
+          </li>
+        )}
         {!user ? (
           <li className={classes["header-link"]}>
             <Link to="/login">SignIn</Link>
           </li>
         ) : (
           <li className={classes["header-link"]}>
-            <Link to="/" onClick={handleSignOut}>Sign Out</Link>
+            <Link to="/" onClick={handleSignOut}>
+              Sign Out
+            </Link>
           </li>
         )}
       </div>
